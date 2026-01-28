780a9fbb37c7fed5887d486785e7a7fd5df71c45
Jack-1235 <Jacko1259mas@gmail.com>
2026-01-28 23:19:18 +0000

    reset session

--- generate.js
+++ generate.js
@@ -6,15 +6,17 @@ const apiId = 31581826;
 const apiHash = "3e159192e5ad7e5052530bb69325994c";
 
 (async () => {
-const client = new TelegramClient(new StringSession(""), apiId, apiHash, { connectionRetries: 5 });
-
-await client.start({
-phoneNumber: async () => await input.text("Number: "),
-password: async () => await input.text("2FA Password (if any): "),
-phoneCode: async () => await input.text("Code: "),
-onError: console.log,
+const client = new TelegramClient(new StringSession("1BAAOMTQ5LjE1NC4xNjcuOTEAUIH2vLewdbRoDQ734df/DiWSe2CVlbcFF15YpJx2ttrwHVsL8ORHZONo1RlGv6R0reeX2tdvpNuLwKWdjiWWapd9DjNYDgGOQnNlkfd7iUAeAcaG64SgBvZZ+ta56VxDxOKZbABhKa+zhUUskNiaMIuARNXaYDA0Cm4gpBNVASA+Y7OW4g8FnkA2p2xUVvCzWiF720LUQvZtzsvSRNO2B8WaOIibDnVjSSRBNEPZtyHs1AxbqQ/+gxVIbqI61b2Qu2yMati2SE6+Q3PIAAIEPKw/uZiZ92Y8xyHYhNdhBQJje4mlK+dPWXZfkwm+Of8/RoFcwufP0BRrrCmG2dx/18k="), apiId, apiHash, {
+connectionRetries: 5,
 });
 
-console.log("\n🔥 COPY THIS STRING:");
+await client.start({
+phoneNumber: async () => await input.text("Phone number: "),
+password: async () => await input.text("2FA password (if any): "),
+phoneCode: async () => await input.text("Code from Telegram: "),
+onError: (err) => console.log(err),
+});
+
+console.log("\nYOUR NEW STRING SESSION:\n");
 console.log(client.session.save());
 })();
